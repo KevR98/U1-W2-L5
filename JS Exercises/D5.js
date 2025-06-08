824dc59ec40ff48eae19a413cb0eb3ff8942bd5a
KevR98 <ramil.kevin98@gmail.com>
2025-06-08 17:16:49 +0200

    finish

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -108,9 +108,21 @@ for (i = 0; i < cars.length; i++) {
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
 */
+
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ];
+let findNumber = 0;
+
+console.log(numericArray);
+
+while (findNumber < numericArray.length) {
+  console.log(numericArray[findNumber]);
+  if (numericArray[findNumber] === 32) {
+    break;
+  }
+  findNumber++;
+}
 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
@@ -118,4 +130,100 @@ const numericArray = [
     es. [f, b, e] --> [6, 2, 5]
 */
 const charactersArray = ["g", "n", "u", "z", "d"];
+const alfabeto = [
+  "1: a",
+  " 2: b",
+  " 3: c",
+  " 4: d",
+  " 5: e",
+  " 6: f",
+  " 7: g",
+  " 8: h",
+  " 9: i",
+  " 10: l",
+  " 11: m",
+  " 12: n",
+  " 13: o",
+  " 14: p",
+  " 15: q",
+  " 16: r",
+  " 17: t",
+  " 18: u",
+  " 19: v",
+  " 20: z",
+];
 const pos = [];
+let savePos;
+for (i = 0; i < charactersArray.length; i++) {
+  switch (charactersArray[i]) {
+    case "a":
+      savePos = 1;
+      break;
+    case "b":
+      savePos = 2;
+      break;
+    case "c":
+      savePos = 3;
+      break;
+    case "d":
+      savePos = 4;
+      break;
+    case "e":
+      savePos = 5;
+      break;
+    case "f":
+      savePos = 6;
+      break;
+    case "g":
+      savePos = 7;
+      break;
+    case "h":
+      savePos = 8;
+      break;
+    case "i":
+      savePos = 9;
+      break;
+    case "l":
+      savePos = 10;
+      break;
+    case "m":
+      savePos = 11;
+      break;
+    case "n":
+      savePos = 12;
+      break;
+    case "o":
+      savePos = 13;
+      break;
+    case "p":
+      savePos = 14;
+      break;
+    case "q":
+      savePos = 15;
+      break;
+    case "r":
+      savePos = 16;
+      break;
+    case "s":
+      savePos = 17;
+      break;
+    case "t":
+      savePos = 18;
+      break;
+    case "u":
+      savePos = 19;
+      break;
+    case "v":
+      savePos = 20;
+      break;
+    case "z":
+      savePos = 21;
+      break;
+    default:
+      savePos = 0;
+  }
+  pos.push(savePos);
+}
+console.log("Controllo: " + alfabeto);
+console.log(charactersArray);
+console.log(pos);
